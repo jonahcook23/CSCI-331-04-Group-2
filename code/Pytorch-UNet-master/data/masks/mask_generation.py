058d682ccd84000131d5c101f0d35a94f11a5b90
Jonah Cook <jonah.cook23@bishopireton.org>
2025-11-05 14:32:31 -0500

    added generated masks

--- code/Pytorch-UNet-master/data/masks/mask_generation.py
+++ code/Pytorch-UNet-master/data/masks/mask_generation.py
@@ -1,5 +1,6 @@
-# NOTE: this code was not designed to be portable. it was designed to be run once and never touched again
+# NOTE: this code was not designed to be portable. it is horribly designed and it was made to be run once and never touched again
 # if you want to modify it to make your own masks, CHANGE THE FILE PATHS. might need to change some other stuff too idk
+# the models might not even need these masks since it just has the raw YOLOv8 labels, so this might not be necessary, but better safe than sorry
 
 from PIL import Image
 import numpy as np
@@ -12,6 +13,8 @@ label_data = []
 dark_mask_color = [200, 0, 200]
 light_mask_color = [255, 255, 0]
 image_counter = 0
+split_path = ''
+rebuilt_path = ''
 
 with os.scandir(dataset_path) as entries:
     for entry in entries:
@@ -24,10 +27,12 @@ with os.scandir(dataset_path) as entries:
                             print(label.path + " has less than 5 values")
                         else:
                             pixel_data = np.zeros((IMAGE_WIDTH, IMAGE_HEIGHT, 3), dtype=np.uint8)
-                            center_x = label_data[1] * IMAGE_WIDTH
-                            center_y = label_data[2] * IMAGE_HEIGHT
-                            width = label_data[3] * IMAGE_WIDTH
-                            height = label_data[4] * IMAGE_HEIGHT
+                            for x in range(5): # because some label files span multiple lines and have newline characters??? idk bro
+                                label_data[x] = label_data[x].replace('\n', '')
+                            center_x = float(label_data[1]) * IMAGE_WIDTH
+                            center_y = float(label_data[2]) * IMAGE_HEIGHT
+                            width = float(label_data[3]) * IMAGE_WIDTH
+                            height = float(label_data[4]) * IMAGE_HEIGHT
                             for y in range(IMAGE_HEIGHT):
                                 for x in range(IMAGE_WIDTH):
                                     # checking if the pixel is outside the ellipse indicated by the yolov8 label
@@ -38,8 +43,12 @@ with os.scandir(dataset_path) as entries:
                             image = Image.fromarray(pixel_data, "RGB")
                             label_name = os.path.basename(file.name)
                             label_name = label_name[:(len(label_name) - 3)]
-                            image_path = label.path + "/../masks/" + label_name + "png"
-                            image.save(image_path)
+                            split_path = label.path.split('/')
+                            rebuilt_path = ''
+                            for x in range(8):
+                                rebuilt_path += split_path[x] + "/"
+                            rebuilt_path += "masks/" + label_name + "png"
+                            image.save(rebuilt_path)
                             image_counter += 1
-                            print(f'generated {image_counter} masks')
+                            print(f'generated {image_counter} masks: {rebuilt_path}')
                             
